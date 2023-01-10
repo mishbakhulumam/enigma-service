c483b8753c061a7dac0fa6e8a0128a927f00b131
mishbakhul umam <mishbakhulumam@gmail.com>
2023-01-10 21:48:08 +0700

    enigma-service

---
+++ dist/js/script.js
@@ -0,0 +1,38 @@
+
+const initApp = () => {
+    const hamburgerBtn = document.getElementById('hamburger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+        hamburgerBtn.classList.toggle('toggle-btn');
+    }
+
+    hamburgerBtn.addEventListener('click', toggleMenu);
+    mobileMenu.addEventListener('click', toggleMenu);
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
+
+
+// map on load
+function initialize() {
+    var propertiPeta = {
+        center: new google.maps.LatLng(-6.8932616, 112.4427847, 15),
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
+
+    // membuat Marker
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(-6.892048, 112.453798),
+        map: peta
+    });
+
+}
+
+// event jendela di-load
+google.maps.event.addDomListener(window, 'load', initialize);
